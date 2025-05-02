a509e4ddc94f4a2586acfbe9536f84397b420f48
Turner-Christian <keycoderk@gmail.com>
2025-05-02 00:00:55 -0400

    Restarted Project
    Created scene with new sprites
    added movement and rotation to frogger

---
+++ Assets/Scripts/Frogger.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Frogger : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            Move(Vector3.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+            Move(Vector3.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+            Move(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+            Move(Vector3.right);
+        }
+    }
+
+    private void Move(Vector3 direction)
+    {
+        transform.position += direction;
+    }
+}
